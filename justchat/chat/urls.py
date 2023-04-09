509d94927d71ea37eee2cc0e2c1719c10c30f189
MohamedHamed12 <mh1779371@gmail.com>
2023-04-09 16:25:21 +0200

    add signals

--- justchat/chat/urls.py
+++ justchat/chat/urls.py
@@ -5,7 +5,7 @@ from . import views
 from rest_framework.routers import DefaultRouter
 router=DefaultRouter()
 router.register('vehicles',views.viewsets_vehicle)
-router.register('accident',views.viewsets_accident)
+router.register('accidents',views.viewsets_accident)
 urlpatterns = [
     path("", views.index, name="index"),
     path("room/<str:room_name>/", views.room, name="room"),
