c4cc015d377a25c651aa982b4749fd3c67390d02
MohamedHamed12 <90472426+MohamedHamed12@users.noreply.github.com>
2023-04-21 17:46:24 +0200

    Delete justchat directory

--- justchat/chat/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-class Messageform(forms.Form):
-    message = forms.CharField(label='Your name', max_length=100)
-    command=forms.CharField(label='Your name', max_length=100)
