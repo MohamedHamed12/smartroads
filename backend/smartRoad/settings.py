3632731586c9f4af4e2449bf9a963decf463f650
محمد الصوالحي <ms.2052001@gmail.com>
2023-05-19 02:45:13 +0200

    Fix cors error in the backend

--- backend/smartRoad/settings.py
+++ backend/smartRoad/settings.py
@@ -35,8 +35,7 @@ INSTALLED_APPS = [
     "daphne",
     'road',
     'account',
-    
-  
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -44,11 +43,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -160,3 +161,8 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
 }
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173'
+]
+
